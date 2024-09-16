93c57c9cc51d2d00dfd3ab7412d6d5dc8e96121d
elakapmain <notquadrilingual@gmail.com>
2024-09-16 13:12:18 -0500

    Create totals & discounts component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+    import Header from './components/Header.vue';
+    import Totals from './components/Totals.vue';
 </script>
 
 <template>
-    
+    <Header></Header>
+    <Totals></Totals>
 </template>
---
+++ src/components/Totals.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        total: {
+            type: Number,
+            require: true,
+        },
+        discounts: {
+            type: Number,
+            require: true,
+        }
+    })
+</script>
+
+<template>
+    <h3>Cart Totals</h3>
+
+    <h2 id="total">{{ total }}</h2>
+    <h2 id="discounts">{{ discounts }}</h2>
+</template>
