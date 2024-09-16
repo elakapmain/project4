937e15ca7af9c78da160905cc43c2154f204ae68
elakapmain <notquadrilingual@gmail.com>
2024-09-16 13:41:00 -0500

    Get list and remover working

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,12 @@
     import Header from './components/Header.vue';
     import Totals from './components/Totals.vue';
     import CartAdd from './components/CartAdd.vue';
+    import CartList from './components/CartList.vue';
 
     import { computed, ref } from 'vue';
 
     const cartItems = ref([
-        {id: 1, name:'Text1', amount:250, discount: 20},
-        {id: 1, name:'Text2', amount:250, discount: 20},
-        {id: 1, name:'Text3', amount:250, discount: 20},
-        {id: 1, name:'Text4', amount:250, discount: 20},
+        {id: 1, name:'Text1', price:250, discount: 20},
     ]);
 
     const cartDiscounts = computed(() =>{
@@ -20,13 +18,17 @@
 
     const cartTotal = computed(() =>{
         return cartItems.value.reduce((sum, x)=>{
-            return sum+x.amount
+            return sum+x.price
         },0)
     })
 
     const discountedTotal = computed(() =>{
         return cartTotal.value - cartDiscounts.value
     })
+    
+    const handleRemove = (id) => {
+        cartItems.value = cartItems.value.filter((x) => x.id !== id)
+    }
 </script>
 
 <template>
@@ -34,5 +36,6 @@
     <div class="container">
         <Totals :total="cartTotal" :totalWithDiscounts="discountedTotal" :discounts="cartDiscounts"></Totals>
         <CartAdd></CartAdd>
+        <CartList :cartItems="cartItems" @itemRemoved="handleRemove"></CartList>
     </div>
 </template>
---
+++ src/components/CartList.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        cartItems: {
+            type: Array,
+            require: true,
+        }
+    })
+
+    const emit = defineEmits(['itemRemoved'])
+
+    const itemRemoved = (id) => {
+        emit('itemRemoved', id)
+    }
+</script>
+
+<template>
+    <h3>In your cart</h3>
+    <ul id="cart"></ul>
+        <li v-for="x in cartItems" v-bind:key="x.id">
+            {{ x.name }} {{ x.price }}
+            <button @click="itemRemoved(x.id)" class="delete-btn">X</button>
+        </li>
+</template>
